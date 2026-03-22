e1a8613e9e7d240bc18ea9c8ce6494422667a028
ZeroNoLight <moonlightandthevodka@gmail.com>
2026-03-22 19:13:26 +0200

    feat: add auth guard to protect dashboard and pet list routes #9

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { Login } from './components/login/login';
 import { Dashboard } from './components/dashboard/dashboard';
 import { PetList} from "./components/pets/pet-list/pet-list";
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'dashboard', component: Dashboard },
-  { path: 'pets', component: PetList },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+  { path: 'pets', component: PetList, canActivate: [authGuard] },
 ];
