102d3064b5f3bf5889d3fd71d843059b7878af43
Marko Esna <marko.esna@gmail.com>
2026-03-24 10:52:20 +0200

    chore: remove duplicates and consolidate logic

--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CreatePetRequest, Pet, UpdatePetRequest } from '../models/pet.model';
 import { catchError, Observable, of } from 'rxjs';
-import { CreatePetRequest } from '../models/pet.model';
 import { Microchip } from '../models/microchip.model';
 
 @Injectable({
@@ -11,6 +11,26 @@ export class PetService {
   private readonly API_URL = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Pet[]> {
+    return this.http.get<Pet[]>(`${this.API_URL}/pets`);
+  }
+
+  getOne(id: number): Observable<Pet> {
+    return this.http.get<Pet>(`${this.API_URL}/pets/${id}`);
+  }
+
+  create(data: CreatePetRequest): Observable<Pet> {
+    return this.http.post<Pet>(`${this.API_URL}/pets`, data);
+  }
+
+  update(id: number, data: UpdatePetRequest): Observable<Pet> {
+    return this.http.patch<Pet>(`${this.API_URL}/pets/${id}`, data);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL}/pets/${id}`);
+  }
 
   addPet(data: CreatePetRequest) {
     return this.http.post(`${this.API_URL}/pets`, data);
