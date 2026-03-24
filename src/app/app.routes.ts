7e33ff61b52db8de173048caf88604fb6fe5fb44
Marko Esna <marko.esna@gmail.com>
2026-03-24 08:46:52 +0200

    feat: add functionality for microchip creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,18 @@ import { Dashboard } from './components/dashboard/dashboard';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home'; // Uus!
 import { PetFormComponent } from './components/pet-form/pet-form';
 import { Login } from './components/login/login';
+import { MicrochipComponent } from './components/add-microchip/add-microchip';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { 
-    path: 'dashboard', 
-    component: Dashboard, 
+  {
+    path: 'dashboard',
+    component: Dashboard,
     children: [
       { path: '', component: DashboardHomeComponent }, // Vaikimisi vaade
-      { path: 'pets/new', component: PetFormComponent }  // Vormi vaade
-    ]
-  }
+      { path: 'pets/new', component: PetFormComponent }, // Vormi vaade
+      { path: 'microchips/new', component: MicrochipComponent },
+    ],
+  },
 ];
